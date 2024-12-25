fb3c914f6d873836625e5708b12c2006a000e208
Daham2004 <147542538+Daham2004@users.noreply.github.com>
2024-12-25 22:10:40 +0530

    Update App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -18,6 +18,7 @@ import UnauthPage from "./pages/unauth-page";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { checkAuth } from "./store/auth-slice";
+import { Skeleton } from "@/components/ui/skeleton";
 
 
 
@@ -33,7 +34,7 @@ function App() {
     dispatch(checkAuth());
   }, [dispatch]);
 
-  
+    if (isLoading) return <Skeleton className="w-[800] bg-black h-[600px]" />;
 
   console.log(isLoading, user);
 
