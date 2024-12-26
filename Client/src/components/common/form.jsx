d0b3a10912be861995cf1a1a8e4cedecd2b5da80
Daham2004 <147542538+Daham2004@users.noreply.github.com>
2024-12-26 16:41:32 +0530

    Update form.jsx

--- Client/src/components/common/form.jsx
+++ Client/src/components/common/form.jsx
@@ -39,7 +39,6 @@ function CommonForm({
             }
           />
         );
-
         break;
       case "select":
         element = (
@@ -66,7 +65,6 @@ function CommonForm({
             </SelectContent>
           </Select>
         );
-
         break;
       case "textarea":
         element = (
@@ -83,9 +81,7 @@ function CommonForm({
             }
           />
         );
-
         break;
-
       default:
         element = (
           <Input
