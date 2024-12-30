0685935281dc7efb2bd52b0a6bc17396b68b2828
Isuri Jayasinghe <147678760+Bhagijay@users.noreply.github.com>
2024-12-30 07:43:40 +0530

    Update check-auth.jsx

--- Client/src/components/common/check-auth.jsx
+++ Client/src/components/common/check-auth.jsx
@@ -5,48 +5,54 @@ function CheckAuth({ isAuthenticated, user, children }) {
 
   console.log(location.pathname, isAuthenticated);
 
-  if (!isAuthenticated) {
-    if (
-      !location.pathname.includes("/login") &&
-      !location.pathname.includes("/register")
-    ) {
+  if (location.pathname === "/") {
+    if (!isAuthenticated) {
       return <Navigate to="/auth/login" />;
-    }
-  } else {
-    if (location.pathname === "/") {
+    } else {
       if (user?.role === "admin") {
         return <Navigate to="/admin/dashboard" />;
       } else {
         return <Navigate to="/shop/home" />;
       }
     }
+  }
 
-    if (
+  if (
+    !isAuthenticated &&
+    !(
       location.pathname.includes("/login") ||
       location.pathname.includes("/register")
-    ) {
-      if (user?.role === "admin") {
-        return <Navigate to="/admin/dashboard" />;
-      } else {
-        return <Navigate to="/shop/home" />;
-      }
-    }
+    )
+  ) {
+    return <Navigate to="/auth/login" />;
+  }
 
-    if (
-      isAuthenticated &&
-      user?.role !== "admin" &&
-      location.pathname.includes("admin")
-    ) {
-      return <Navigate to="/unauth-page" />;
-    }
-
-    if (
-      isAuthenticated &&
-      user?.role === "admin" &&
-      location.pathname.includes("shop")
-    ) {
+  if (
+    isAuthenticated &&
+    (location.pathname.includes("/login") ||
+      location.pathname.includes("/register"))
+  ) {
+    if (user?.role === "admin") {
       return <Navigate to="/admin/dashboard" />;
+    } else {
+      return <Navigate to="/shop/home" />;
     }
+  }
+
+  if (
+    isAuthenticated &&
+    user?.role !== "admin" &&
+    location.pathname.includes("admin")
+  ) {
+    return <Navigate to="/unauth-page" />;
+  }
+
+  if (
+    isAuthenticated &&
+    user?.role === "admin" &&
+    location.pathname.includes("shop")
+  ) {
+    return <Navigate to="/admin/dashboard" />;
   }
 
   return <>{children}</>;
