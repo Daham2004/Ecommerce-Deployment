ec1b188d42b78c379e24d21aafde202317d8b952
Mohammed Zamrah <10898951@students.plymouth.ac.uk>
2025-01-04 17:28:46 +0530

    Update orders.jsx

--- Client/src/pages/admin-view/orders.jsx
+++ Client/src/pages/admin-view/orders.jsx
@@ -1,1 +1,12 @@
+import AdminOrdersView from "@/components/admin-view/orders";
+
+function AdminOrders() {
+  return (
+    <div>
+      <AdminOrdersView />
+    </div>
+  );
+}
+
+export default AdminOrders;
 
